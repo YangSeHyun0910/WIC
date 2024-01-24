89ea58c6f55e2790e63f4f94fc8893f9c9646458
DESKTOP-R6VDQ39\yangs <yangsehyun1@gmail.com>
2024-01-24 22:21:42 +0900

    commin email change test

--- wic/src/Body/SearchAndLocation/Content/StoreSearch.jsx
+++ wic/src/Body/SearchAndLocation/Content/StoreSearch.jsx
@@ -3,7 +3,7 @@ import './StoreSearch.css';
 function StoreSearch() {
 
     return(
-        <input className="storeSearchBox" placeholder='가게 검색' />
+        <input className="storeSearchBox" placeholder='가게 검색창' />
     )
 }
 export default StoreSearch;
