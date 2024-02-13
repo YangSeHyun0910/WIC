879e2855f592bba7f596540aeddf598f6551d9c6
DESKTOP-R6VDQ39\yangs <yangsehyun1@gmail.com>
2024-02-13 23:31:18 +0900

    결과영역 수정

--- wic/src/Body/ResultCafe/ResultCafe.jsx
+++ wic/src/Body/ResultCafe/ResultCafe.jsx
@@ -3,7 +3,9 @@ import './ResultCafe.css';
 function ResultCafe() {
 
     return(
-        <div className="resultCafe">결과가게</div>
+        <div className='resultArea'>
+            <div className="resultCafe">결과가게</div>
+        </div>
     )
 }
 export default ResultCafe;
